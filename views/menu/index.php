154f6525a706bd1d0fa78bc56ff1d17ff471236a
egor <egorkonopka93@gmail.com>
2020-11-27 16:09:53 +0200

    fridy 27.11.2020

--- views/menu/index.php
+++ views/menu/index.php
@@ -2,102 +2,52 @@
 use yii\helpers\Html;
 use yii\helpers\Url
 ?>
-<div class="main-content">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-
-<?php foreach($res as $key => $value): ?>
-                <article class="post post-list">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="post-thumb" style="background-color: black">
-                            	<?php if($key=='port_place'){
-                                echo '<a href="blog.html"><img src="/public/images/PORTPLACE.png" alt="" class="pull-left"></a>';
-                            	}else{
-                            	echo '<a href="blog.html"><img src="/public/images/blog-grid.jpg" alt="" class="pull-left"></a>';	
-                        		}?>
-                                <a href="blog.html" class="post-thumb-overlay text-center">
-                                    <div class="text-uppercase text-center"><?= $value->name ?></div>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css">
+<div class="container">
+    <?php foreach ($res as $ress) :?>
+            <div class="row container d-flex justify-content-center">
+                <div class="col-xl-6 col-md-12">
+                    <div class="card user-card-full">
+                        <div class="row m-l-0 m-r-0">
+                            <div class="col-sm-4 bg-c-lite-green user-profile">
+                                <a href="<?= Url::to(['menu/view','id'=>$ress->id_res]) ?>">
+                                <div class="card-block text-center text-white">
+                                    <div class="m-b-25"> <img src="/public/images/blog-grid.jpg" class="img-radius" alt="User-Profile-Image"> </div>
+                                    <h6 class="f-w-600"><a href="<?= Url::to(['menu/view','id'=>$ress->id_res]) ?>"><?= $ress->name?></a></h6>
+                                </div>
                                 </a>
                             </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="post-content">
-                                <header class="entry-header text-uppercase">
-
-                                    <h1 class="entry-title"><a href="<?= Url::to(['menu/view','id'=>$value->id_res]) ?>"><?= $value->name ?></a></h1>
-                                </header>
-                                <div class="entry-content">
-                                    <p><?= $value->content ?>                                    
-                                    </p>
-                                </div> 
-                                <div class="social-share">
-                                    <span class="social-share-title pull-left text-capitalize">Время работы <?=substr($value->start_at,0, 5);?>  -  <?=substr($value->end_at,0, 5);?>
-                                        
-                                    <?php $timeNow = date("H:i:s");?>
-                                    <?php if($timeNow > $value->start_at & $timeNow < $value->end_at):?>
-                                        <button type="button" class="btn btn-success">Открыто</button>
-                                    <?php else : ?>
-                                        <button type="button" class="btn btn-danger">Закрыто</button>
-                                    <?php endif;?>
-
-
-                                    </span>
-
+                            <div class="col-sm-8">
+                                <div class="card-block">
+                                    <h6 align="center" class="p-b-5 b-b-default f-w-600">Information</h6>
+                                    <div class="row">
+                                        <div align="center" class="col-sm-6">
+                                            <p class="m-b-10 f-w-600">location</p>
+                                            <h6 class="text-muted f-w-400">address</h6>
+                                        </div>
+                                        <div align="center" class="col-sm-6">
+                                            <p class="m-b-10 f-w-600">Phone</p>
+                                            <h6 class="text-muted f-w-400"><?= $ress->phone?></h6>
+                                        </div>
+                                    </div>
+                                    <div class="b-b-default"></div>
+                                    <div class="row">
+                                        <div align="center" class="col-sm-12">
+                                            <p class="m-b-10 f-w-600">Доставка</p>
+                                            <h6 class="text-muted f-w-400">TAXI (бесплатно от 300грн)</h6>
+                                        </div>
+                                    </div>
+                                    <div class="b-b-default"></div>
+                                        <ul align="center" class="social-link list-unstyled">
+                                        <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
+                                        <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
+                                        <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </article>
-<?php endforeach; ?>
-<!-- <div class="sale-wrap">
-  <div class="sale-inner">
-            <div class="centrr">
-         asd asd
-        </div>
-    <div class="leftt">
-      
-    </div>
-
-    <div class="rightt">
-      
-    </div>
-  </div>
-</div> -->
-
-
-                <!-- <article class="post post-list">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="post-thumb">
-                                <a href="blog.html"><img src="/public/images/blog-grid.jpg" alt="" class="pull-left"></a>
-
-                                <a href="blog.html" class="post-thumb-overlay text-center">
-                                    <div class="text-uppercase text-center">Evropeiskiy</div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="post-content">
-                                <header class="entry-header text-uppercase">
-
-                                    <h1 class="entry-title"><a href="blog.html">Evropeiskiy</a></h1>
-                                </header>
-                                <div class="entry-content">
-                                    <p>Еда с рестика  текст текст текст текст текст текст текст текст ТОП Еда с рестика 
-                                    </p>
-                                </div>
-                                <div class="social-share">
-                                    <span class="social-share-title pull-left text-capitalize">Работает 10:00 - 23:00</span>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </article> -->
-                
+                </div>
             </div>
-        </div>
-    </div>
+    <?php endforeach ;?>
 </div>
